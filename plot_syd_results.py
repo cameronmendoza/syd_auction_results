67ed0735ed62b6b4bbe6ae49c81bd980768702e4
Cameron Mendoza <msg.cam@gmail.com>
2016-01-15 20:43:59 +1100

    Add first entry for 2016 and go back to old plot style.

--- plot_syd_results.py
+++ plot_syd_results.py
@@ -10,7 +10,7 @@ def graph():
 
 	ax1 = fig.add_subplot(1, 1, 1, axisbg='white')
 
-	plt.plot_date(x=date, y=cr, fmt='')
+	plt.plot_date(x=date, y=cr, fmt='ro')
 
 	plt.title('Sydney\'s Clearance rate')
 	plt.ylabel('Clearance rate (%)')
