83d326015384e211a8ed9c0c539664a1f690c90f
Cameron Mendoza <msg.cam@gmail.com>
2016-04-09 21:55:07 +1000

    Update plot+results.

--- plot_syd_results.py
+++ plot_syd_results.py
@@ -11,7 +11,7 @@ def graph():
 
 	ax1 = fig.add_subplot(1, 1, 1, axisbg='white')
 
-	plt.plot_date(x=date, y=cr, marker='x', color='r')
+	plt.plot_date(x=date, y=cr, marker='x', color='b')
 	plt.plot_date(x=date, y=cr, marker='', linestyle='-', color='b')
 
 	plt.title('Sydney\'s Clearance rate')
