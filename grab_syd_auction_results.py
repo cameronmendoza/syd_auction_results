155ed2ad15f772f662ea4bc59b2cc7f042f30c2e
Cameron Mendoza <hello@cameronmendoza.id.au>
2017-05-07 16:59:13 +1000

    Update scraper. Update results 07052017.

--- grab_syd_auction_results.py
+++ grab_syd_auction_results.py
@@ -2,7 +2,7 @@ from lxml import html
 import requests
 
 def get_value_without_key(generated_list):
-	return generated_list[0].split(": ")[1]
+	return generated_list[0].split(" ")[0]
 
 # Currently, updated weekly - ending on the Sunday
 page = requests.get('https://www.realestate.com.au/auction-results/nsw')
@@ -10,13 +10,13 @@ tree = html.fromstring(page.content)
 
 # Create list from raw XPath query
 date = tree.xpath('/html/body/section/section[1]/p/time/@datetime')
-clearance_rate_percentage = tree.xpath('/html/body/main/section[1]/div[2]/div[1]/text()')
+clearance_rate_percentage = tree.xpath('/html/body/main/section[1]/div[3]/div[1]/span[1]/text()')
 total_sched_auctions = tree.xpath('/html/body/main/section[1]/div[1]/div[1]/text()')
 sold_prior_to_auction = tree.xpath('/html/body/main/section[1]/ul/li[1]/text()')
-passed_in = tree.xpath('/html/body/main/section[1]/ul/li[2]/text()')
-sold_at_auction = tree.xpath('/html/body/main/section[1]/ul/li[3]/text()')
+passed_in = tree.xpath('/html/body/main/section[1]/ul/li[5]/text()')
+sold_at_auction = tree.xpath('/html/body/main/section[1]/ul/li[2]/text()')
 withdrawn = tree.xpath('/html/body/main/section[1]/ul/li[4]/text()')
-sold_after_auction = tree.xpath('/html/body/main/section[1]/ul/li[5]/text()')
+sold_after_auction = tree.xpath('/html/body/main/section[1]/ul/li[3]/text()')
 
 date_value = date[0]
 clearance_rate_percentage_value = str(clearance_rate_percentage[0]).replace('%', '')
